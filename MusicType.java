3f7d9d5599c94e10d05d3a9cc1b70a2d8a519cfd
Matthew Brown <mattd429@gmail.com>
2017-04-14 13:09:34 -0400

    Update MusicType.java

--- MusicType.java
+++ MusicType.java
@@ -1,1 +1,23 @@
+package com.exerise.theLoops;
+/**
+  * Created by MBROWN
+  */
+public class MusicType {
+  private String categoryMusic;
+  
+  public MusicType(String categoryMusic)
+  {
+    this.categoryMusic = categoryMusic;
+  }
+  public String getCategoryMusic() { return this.categoryMusic; }
+  
+  public void setCategoryMusic = categoryMusic;
+  }
 
+@Override
+public String toString() {
+  return "MusicType{" + 
+          "categoryMusic='" + categoryMusic + "'\n";
+
+  }
+}

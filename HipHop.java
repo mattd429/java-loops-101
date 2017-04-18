37c806c8b953ddd36a176e0fcc7184080670ba89
Matthew Brown <mattd429@gmail.com>
2017-04-17 21:26:01 -0400

    Create HipHop.java

---
+++ HipHop.java
@@ -0,0 +1,31 @@
+package com.excerise.theLoops
+/*
+ *Create by MBrown
+ *
+ */
+ 
+ public class HipHop {
+        private String tours;
+        Private double trackTime;
+        private double venuePrice;
+        private String promotorName;
+        private String manager;
+        private String artist;
+        private String recordLabel;
+        private double numberOfSales;
+        private int numberOfSongs;
+        private Date releaseDate;
+        
+        public HipHop(String tours, String manager, int day, int month, int year)
+        {
+          //had a super here but removed since it is not an inheritance
+          releaseDate = new Date(day, month, year);
+          this.tours = tours;
+          this.manager = manager;
+          
+        }
+        public HipHop(double trackTime, double venuePrice, string artist, string manager) 
+        {
+          this.
+        }
+ }

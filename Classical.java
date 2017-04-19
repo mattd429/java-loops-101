1a375d84db4878181ac720e022f7cd94a8acf6f0
Matthew Brown <mattd429@gmail.com>
2017-04-18 21:11:04 -0400

    Update Classical.java

--- Classical.java
+++ Classical.java
@@ -1,1 +1,28 @@
-
+package com.excerise.theLoops
+/*
+ *Create by MBrown 
+ */
+ public class Classical extends MusicType{
+     private String author;
+     private double playTime;
+ 
+     public(String author, double playTime) 
+     {
+          super("Classical")
+          this.author = author;
+          this.playTime = playTime;
+     
+     }
+     public String getAuthor() 
+     {
+     return this.author;
+     }
+     public double getPlayTime() 
+     {
+     return this.playTime;
+     }
+     public String toString() 
+     {
+     return this.author + " " + this.playTime;
+     }
+ }
